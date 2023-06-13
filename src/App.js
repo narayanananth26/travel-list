41e7284ac1c80482371c745d7f078402bf241065
Ananth Narayan <narayanananth26@gmail.com>
2023-06-13 13:08:33 +0530

    implement item packed functionality

--- src/App.js
+++ src/App.js
@@ -12,11 +12,23 @@ export default function App() {
 		setItems((items) => items.filter((item) => item.id !== id));
 	}
 
+	function handleToggleItem(id) {
+		setItems((items) =>
+			items.map((item) =>
+				item.id === id ? { ...item, packed: !item.packed } : item
+			)
+		);
+	}
+
 	return (
 		<div className="app">
 			<Logo />
 			<Form onAddItems={handleAddItems} />
-			<PackingList items={items} onDeleteItem={handleDeleteItem} />
+			<PackingList
+				items={items}
+				onDeleteItem={handleDeleteItem}
+				onToggleItem={handleToggleItem}
+			/>
 			<Stats />
 		</div>
 	);
@@ -71,7 +83,7 @@ function Form({ onAddItems }) {
 	);
 }
 
-function PackingList({ items, onDeleteItem }) {
+function PackingList({ items, onDeleteItem, onToggleItem }) {
 	return (
 		<div className="list">
 			<ul>
@@ -80,6 +92,7 @@ function PackingList({ items, onDeleteItem }) {
 						item={item}
 						key={item.id}
 						onDeleteItem={onDeleteItem}
+						onToggleItem={onToggleItem}
 					/>
 				))}
 			</ul>
@@ -87,9 +100,16 @@ function PackingList({ items, onDeleteItem }) {
 	);
 }
 
-function Item({ item, onDeleteItem }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
 	return (
 		<li>
+			<input
+				type="checkbox"
+				value={item.packed}
+				onChange={() => {
+					onToggleItem(item.id);
+				}}
+			/>
 			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
 				{item.quantity} {item.description}
 			</span>
